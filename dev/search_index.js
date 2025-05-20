3f5687a677423d271c7932d8f45b9febca96e9b0
Documenter.jl <documenter@juliadocs.github.io>
2025-05-20 22:18:08 +0000

    build based on c528386

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#PythonDictMacros","page":"Home","title":"PythonDictMacros","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PythonDictMacros.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
